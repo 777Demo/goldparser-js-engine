af1638685eb5a0169aa41122e1bf41f0d1ec7ba4
777crew <38739049+777crew@users.noreply.github.com>
2025-02-05 21:51:54 -0500

    Initial commit

---
+++ examples/compile.bat
@@ -0,0 +1,12 @@
+cls
+set gram=Simple2
+::call GOLDbuild -export %gram%.grm %gram%.egt %gram%.log
+call GOLDbuild -export calc.grm calc.egt %gram%.log
+::call GOLDprog %gram%.egt ../egt/JavascriptEngine.pgt %gram%.js
+::call GOLDprog %gram%.egt "C - Kessels - Engine grammar.h.pgt" %gram%.h
+::call GOLDprog %gram%.egt "C - Kessels - Engine grammar.c.pgt" %gram%.c
+::call GOLDprog %gram%.egt PHPEngine.pgt %gram%.php
+::call GOLDprog grm.cgt ../cgt/JavascriptEngine.pgt grm.js
+call GOLDprog calc.egt ../templates/egt/BaseJSEngine.pgt BaseCalc.js
+call GOLDprog calc.egt ../templates/egt/ImpJSEngineWithSwitch.pgt  Calc.js
+call GOLDprog calc.egt ../templates/egt/TestJSEngine.pgt TestCalc.js
